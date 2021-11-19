9340e707618f81086f041c39cd5eba438c31f194
pterolycus9pn <pngwira7@gmail.com>
2021-11-19 17:09:51 +0200

    Updated career

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import './App.css';
-import Hline from './Hline'
-import About from './About'
-import Education from './Education'
-import Carreer from './Carreer'
-import Skills from './Skills'
-import Footer from './Footer'
+import Hline from './Hline';
+import About from './About';
+import Education from './Education';
+import Carreer from './Carreer';
+import Skills from './Skills';
+import Footer from './Footer';
 // import './Navbar/NavbarElements';
 // import './Navbar/'
 
 function App() {
-
   return (
     <div className="App">
       <header className="App-header">
-          <h1>Pepulani Ngwira</h1>
-          <Hline color="white" width="60%"/>
-          <h2 className="sub-header">Interactive resume</h2>
+        <h1>Pepulani Ngwira</h1>
+        <Hline color="white" width="60%" />
+        <h2 className="sub-header">Interactive resume</h2>
       </header>
       <div className="prof">
         <h1 className="profile-header">Profile</h1>
         <p className="prof">
-          I am a systems developer. I design web applications and systems
-          that allow organizations to make informed and professional business 
+          I am a systems developer. I design web applications and systems that
+          allow organizations to make informed and professional business
           decisions.
         </p>
         <Hline color="grey" width="60%" />
-        <About /> 
+        <About />
       </div>
       <div className="Education">
         <h1 className="Edu">Education</h1>
@@ -33,7 +32,7 @@ function App() {
         <Education />
       </div>
       <div className="carreer">
-        <h1 className="Car">Carreer</h1>
+        <h1 className="Car">Career</h1>
         <Hline color="grey" width="60%" />
         <Carreer />
       </div>
@@ -48,7 +47,6 @@ function App() {
         <Hline color="grey" width="60%" />
         <Footer />
       </footer>
-
     </div>
   );
 }
--- src/Skills.js
+++ src/Skills.js
@@ -1,27 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 function Skills() {
-    return (
-        <div>
-            <table id="skills">
-                <tr>
-                    <td id="php">PHP</td>
-                    <td id="react">React(Javascript library)</td>
-                    <td id="html">HTML</td>
-                </tr>
-                <tr>
-                    <td id="mysql">MySQL</td>
-                    <td id="mssql">MSSQL</td>
-                    <td id="css">CSS</td>
-                </tr>
-                <tr>
-                    <td id="linux">Linux</td>
-                    <td id="git">Software version control system (git)</td>
-                </tr>
-            </table>
-
-        </div>
-    )
+  return (
+    <div>
+      <table id="skills">
+        <tr>
+          <td id="php">PHP</td>
+          <td id="react">React (Javascript library)</td>
+          <td id="html">HTML</td>
+        </tr>
+        <tr>
+          <td id="mysql">MySQL</td>
+          <td id="mssql">MSSQL</td>
+          <td id="css">CSS</td>
+        </tr>
+        <tr>
+          <td id="linux">Linux</td>
+          <td id="git">Software version control system (git)</td>
+        </tr>
+      </table>
+    </div>
+  );
 }
 
-export default Skills
+export default Skills;
