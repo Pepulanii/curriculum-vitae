7fe9f219f1dd7b732f5a02f619c0bf29a7aa232d
pterolycus9pn <pngwira7@gmail.com>
2021-11-24 20:23:02 +0200

    Update package.json

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import profpic from '../images/profpic.png';
 import { Link } from 'react-router-dom';
+import linkedin from '../images/linkedin.svg';
+import facebook from '../images/facebook.svg';
+import twitter from '../images/twitter.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faFacebook,
@@ -12,20 +15,6 @@ function Profile() {
   return (
     <div>
       <div>
-        {/* <div className="card">
-          <div className="card-image">
-            <img className="activator" src={profpic} alt="my profile" />
-            <Link className="btn-floating halfway-fab waves-effect wave-light red">
-              <i className="material-icons activator" />
-              more_vert
-            </Link>
-          </div>
-          <div className="card-content">
-            <span className="card-title activator grey-text text-darken-4">
-              Pepulani Ngwira
-            </span>
-            <p>Systems Developer</p>
-          </div> */}
         <div className="card">
           <div className="card-image waves-effect waves-block waves-light">
             <img className="activator" src={profpic} />
@@ -44,9 +33,9 @@ function Profile() {
               Social Media <i class="fas fa-times" />
             </span>
             <p className="flex-container">
-              <i class="fab fa-facebook-f" />
-              <i class="fab fa-twitter" />
-              <i class="fab fa-linkedin-in" />
+              <img src={facebook} style={{ height: '20px', width: '20px' }} />
+              <img src={linkedin} style={{ height: '20px', width: '20px' }} />
+              <img src={twitter} style={{ height: '20px', width: '20px' }} />
             </p>
           </div>
         </div>
