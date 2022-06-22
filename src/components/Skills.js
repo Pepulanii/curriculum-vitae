1ace50d46ca6c33b4e01bb7ab2808f028a3b166f
Pepulani <pngwira7@gmail.com>
2022-06-22 07:27:54 +0200

    Updated Skills
    updated skills.js: rearranged the skills content

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -35,6 +35,24 @@ function Skills() {
         </h6>
         <div className="row mt-top">
           <div className="col s6">
+            <p>Javascript (REACT)</p>
+            <div className="progress grey lighten-1">
+              <div className="determinate blue" style={{ width: "75%" }}></div>
+            </div>
+          </div>
+          <div className="col s6">
+            <p>WORDPRESS</p>
+            <div className="progress grey lighten-1">
+              <div className="determinate blue" style={{ width: "80%" }}></div>
+            </div>
+          </div>
+          <div className="col s6">
+            <p>PHP</p>
+            <div className="progress grey lighten-1">
+              <div className="determinate blue" style={{ width: "60%" }}></div>
+            </div>
+          </div>
+          <div className="col s6">
             <p>HTML</p>
             <div className="progress grey lighten-1">
               <div className="determinate blue" style={{ width: "75%" }}></div>
@@ -44,18 +62,6 @@ function Skills() {
             <p>CSS</p>
             <div className="progress grey lighten-1">
               <div className="determinate blue" style={{ width: "90%" }}></div>
-            </div>
-          </div>
-          <div className="col s6">
-            <p>Javascript (REACT)</p>
-            <div className="progress grey lighten-1">
-              <div className="determinate blue" style={{ width: "75%" }}></div>
-            </div>
-          </div>
-          <div className="col s6">
-            <p>PHP</p>
-            <div className="progress grey lighten-1">
-              <div className="determinate blue" style={{ width: "60%" }}></div>
             </div>
           </div>
           <div className="col s6">
@@ -74,12 +80,6 @@ function Skills() {
             <p>GIT</p>
             <div className="progress grey lighten-1">
               <div className="determinate blue" style={{ width: "50%" }}></div>
-            </div>
-          </div>
-          <div className="col s6">
-            <p>WORDPRESS</p>
-            <div className="progress grey lighten-1">
-              <div className="determinate blue" style={{ width: "80%" }}></div>
             </div>
           </div>
         </div>
